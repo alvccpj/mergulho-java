7e1aebf56c36d7db2855452e5db53c2fb4148b64
Álvaro Jordão <alvccpj@gmail.com>
2023-07-19 13:34:21 -0300

    starting Java's bootcamp

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "Álvaro Jordão";
+        System.out.println("Hello, World!");
+        System.out.println("Meu nome é: " +nomeCompleto);
+    }
+}
